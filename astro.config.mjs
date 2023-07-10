5ebb3ca1eff49dde33bc5b3a2ed5512f3913eea8
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2023-07-10 19:51:09 +0200

    add analog astro and angular example component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import angular from "@analogjs/astro-angular";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [angular()],
+});
