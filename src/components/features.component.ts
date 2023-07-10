e5225114c8ac0e01f5cf060aec9c5835bf15106b
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2023-07-10 20:00:14 +0200

    add rx-angular example

---
+++ src/components/features.component.ts
@@ -0,0 +1,22 @@
+import { Component } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
+import { RxLet } from "@rx-angular/template/let";
+
+@Component({
+  selector: "app-features",
+  standalone: true,
+  imports: [RxLet],
+  template: `
+    <p>Feaures</p>
+
+    <div *rxLet="count$; let count">
+      {{ count }}
+    </div>
+
+    <button (click)="count$.next(count$.getValue() + 1)">+1</button>
+    <button (click)="count$.next(count$.getValue() - 1)">-1</button>
+  `,
+})
+export class FeaturesComponent {
+  count$ = new BehaviorSubject(0);
+}
